32a3f230aea1c722db9ee16775ab101ad8d41d2f
Stanislav <stas.lemeshonok98@mail.ru>
2022-03-21 19:54:18 +0300

    Create store

--- react-app-1/src/index.js
+++ react-app-1/src/index.js
@@ -1,21 +1,22 @@
 import './index.css';
-import state from './redux/state.js';
+import store from './redux/state.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {addPost} from './redux/state.js';
-import {addMessage} from './redux/state.js';
-import {updateNewPostText} from './redux/state.js';
-import {subscribe} from './redux/state.js';
 
 
 let rerenderEntireTree = (state) => {
 
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} addMessage={addMessage} updateNewPostText={updateNewPostText}/>
+        <App 
+            state={state}
+            addPost={store.addPost.bind(store)}
+            addMessage={store.addMessage.bind(store)}
+            updateNewPostText={store.updateNewPostText.bind(store)}
+        />
     </React.StrictMode>,
     document.getElementById('root')
   );
@@ -23,6 +24,6 @@ let rerenderEntireTree = (state) => {
   reportWebVitals();
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
--- react-app-1/src/redux/state.js
+++ react-app-1/src/redux/state.js
@@ -1,50 +1,56 @@
-let rerenderEntireTree = () => {
-    console.log('State wtf')
-};
-
-let state = {
-    profilePage: {
-        postsData: [
-            {id: '0', message: 'Hi, amogus', likesCount: 0},
-            {id: '1', message: 'Hardcoding', likesCount: 16},
-        ],
-        newPostText: '',
+let store = {
+    _callSubscriber() {
+        console.log('State wtf')
     },
-    messagesPage: {
-        messagesData: [
-            {message: 'Hi', id: '0'},
-            {message: 'Bruh', id: '1'},
-            {message: 'Wtb', id: '2'},
-            {message: 'Egzi', id: '3'},
-        ],
-        dialogsData: [
-            {name: 'Chel 0', id: '1'},
-            {name: 'Chel 1', id: '2'},
-            {name: 'Chel 2', id: '3'},
-        ],
+
+    _state: {
+        profilePage: {
+            postsData: [
+                {id: '0', message: 'Hi, amogus', likesCount: 0},
+                {id: '1', message: 'Hardcoding', likesCount: 16},
+            ],
+            newPostText: '',
+        },
+        messagesPage: {
+            messagesData: [
+                {message: 'Hi', id: '0'},
+                {message: 'Bruh', id: '1'},
+                {message: 'Wtb', id: '2'},
+                {message: 'Egzi', id: '3'},
+            ],
+            dialogsData: [
+                {name: 'Chel 0', id: '1'},
+                {name: 'Chel 1', id: '2'},
+                {name: 'Chel 2', id: '3'},
+            ],
+        },
+    },
+
+    getState() {
+        return this._state;
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    addPost() {
+        let newPost = {id: 2, message: this._state.profilePage.newPostText, likesCount: 0};
+        this._state.profilePage.postsData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+
+    addMessage(dialogMessage) {
+        let newMessage = {message: dialogMessage, id: '4'};
+        this._state.messagesPage.messagesData.push(newMessage);
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer; // observer (pattern)
     },
 };
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-};
-
-export const addPost = () => {
-    let newPost = {id: 2, message: state.profilePage.newPostText, likesCount: 0};
-    state.profilePage.postsData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-};
-
-export const addMessage = (dialogMessage) => {
-    let newMessage = {message: dialogMessage, id: '4'};
-    state.messagesPage.messagesData.push(newMessage);
-    rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; // observer (pattern)
-};
-
-export default state;
+export default store;
